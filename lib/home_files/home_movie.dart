32a03b664d3a0540827d08853c8dfa8b0ccc85b9
Gihong <gihong0409@naver.com>
2023-08-17 17:11:34 +0200

    컨텐츠 카드 작업 중

--- lib/home_files/home_movie.dart
+++ lib/home_files/home_movie.dart
@@ -24,87 +24,10 @@ class _HomeMovie extends State<HomeMovie> {
               children: [
                 Expanded(
                   child: Container(
-                    padding: const EdgeInsets.all(10),
-                    height: 240,
-                    color: const Color.fromARGB(255, 199, 199, 199),
-                    child: Column(
-                      children: [
-                        const Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            Text(
-                              "박스오피스 순위",
-                              style: TextStyle(
-                                  fontSize: 25, fontWeight: FontWeight.bold),
-                            ),
-                            Text(
-                              "＞",
-                              style: TextStyle(
-                                fontSize: 17,
-                                fontWeight: FontWeight.bold,
-                                color: Color.fromARGB(255, 116, 116, 116),
-                              ),
-                            ),
-                          ],
-                        ),
-                        Expanded(
-                          //텍스트 아래래
-                          child: SingleChildScrollView(
-                            //오른쪽으로 넘기기
-                            scrollDirection: Axis.horizontal,
-                            child: Expanded(
-                                child: Row(
-                              mainAxisAlignment: MainAxisAlignment.start,
-                              children: [
-                                Container(
-                                  width: 110,
-                                  color: Colors.blue,
-                                  margin: const EdgeInsets.only(right: 10),
-                                ),
-                                Container(
-                                  width: 110,
-                                  color: const Color.fromARGB(255, 85, 85, 85),
-                                  margin: const EdgeInsets.only(right: 10),
-                                ),
-                                Container(
-                                  width: 110,
-                                  color: const Color.fromARGB(255, 138, 56, 56),
-                                  margin: const EdgeInsets.only(right: 10),
-                                ),
-                                Container(
-                                  width: 110,
-                                  color: const Color.fromARGB(255, 56, 97, 138),
-                                  margin: const EdgeInsets.only(right: 10),
-                                ),
-                                Container(
-                                  width: 110,
-                                  color: const Color.fromARGB(255, 76, 138, 56),
-                                  margin: const EdgeInsets.only(right: 10),
-                                ),
-                                Container(
-                                  width: 110,
-                                  color:
-                                      const Color.fromARGB(255, 135, 56, 138),
-                                  margin: const EdgeInsets.only(right: 10),
-                                ),
-                                Container(
-                                  width: 110,
-                                  color:
-                                      const Color.fromARGB(255, 138, 104, 56),
-                                  margin: const EdgeInsets.only(right: 10),
-                                ),
-                                Container(
-                                  width: 110,
-                                  color: const Color.fromARGB(255, 97, 56, 138),
-                                  margin: const EdgeInsets.only(right: 10),
-                                ),
-                              ],
-                            )),
-                          ),
-                        )
-                      ],
-                    ),
-                  ),
+                      padding: const EdgeInsets.all(10),
+                      height: 240,
+                      color: const Color.fromARGB(255, 199, 199, 199),
+                      child: const ContentsCard(title: '박스오피스 순위111~1!')),
                 ),
               ],
             ),
@@ -116,53 +39,90 @@ class _HomeMovie extends State<HomeMovie> {
 }
 
 class ContentsCard extends StatelessWidget {
-  const ContentsCard({super.key});
+  const ContentsCard({required this.title, Key? key}) : super(key: key);
+  final String title;
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
 
     return Scaffold(
       body: Center(
-          child: Row(
-        mainAxisAlignment: MainAxisAlignment.start,
-        children: [
-          Expanded(
-            child: Container(
-              padding: const EdgeInsets.all(10),
-              height: 240,
-              color: const Color.fromARGB(255, 199, 199, 199),
-              child: Column(
-                children: [
-                  const Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Text(
-                        "박스오피스 순위1",
-                        style: TextStyle(
-                            fontSize: 25, fontWeight: FontWeight.bold),
-                      ),
-                      Expanded(
-                        child: Text(
-                          "＞",
-                          style: TextStyle(
-                            fontSize: 40,
-                            fontWeight: FontWeight.bold,
-                            color: Color.fromARGB(255, 116, 116, 116),
-                          ),
-                        ),
-                      ),
-                    ],
+        child: Column(
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  title, //여기서 오류
+                  style: const TextStyle(
+                      fontSize: 25, fontWeight: FontWeight.bold),
+                ),
+                const Text(
+                  "＞",
+                  style: TextStyle(
+                    fontSize: 17,
+                    fontWeight: FontWeight.bold,
+                    color: Color.fromARGB(255, 116, 116, 116),
                   ),
-                  Container(
-                    color: Colors.black,
-                    child: const Text("data"),
-                  )
-                ],
-              ),
+                ),
+              ],
             ),
-          ),
-        ],
-      )),
+            Expanded(
+              //텍스트 아래래
+              child: SingleChildScrollView(
+                //오른쪽으로 넘기기
+                scrollDirection: Axis.horizontal,
+                child: Expanded(
+                    child: Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    Container(
+                      width: 110,
+                      color: Colors.blue,
+                      margin: const EdgeInsets.only(right: 10),
+                    ),
+                    Container(
+                      width: 110,
+                      color: const Color.fromARGB(255, 85, 85, 85),
+                      margin: const EdgeInsets.only(right: 10),
+                    ),
+                    Container(
+                      width: 110,
+                      color: const Color.fromARGB(255, 138, 56, 56),
+                      margin: const EdgeInsets.only(right: 10),
+                    ),
+                    Container(
+                      width: 110,
+                      color: const Color.fromARGB(255, 56, 97, 138),
+                      margin: const EdgeInsets.only(right: 10),
+                    ),
+                    Container(
+                      width: 110,
+                      color: const Color.fromARGB(255, 76, 138, 56),
+                      margin: const EdgeInsets.only(right: 10),
+                    ),
+                    Container(
+                      width: 110,
+                      color: const Color.fromARGB(255, 135, 56, 138),
+                      margin: const EdgeInsets.only(right: 10),
+                    ),
+                    Container(
+                      width: 110,
+                      color: const Color.fromARGB(255, 138, 104, 56),
+                      margin: const EdgeInsets.only(right: 10),
+                    ),
+                    Container(
+                      width: 110,
+                      color: const Color.fromARGB(255, 97, 56, 138),
+                      margin: const EdgeInsets.only(right: 10),
+                    ),
+                  ],
+                )),
+              ),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
